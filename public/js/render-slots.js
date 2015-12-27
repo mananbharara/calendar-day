354c87b7bab20afc04d38318d27f891b4081970c
Manan <manan.bharara@gmail.com>
2015-12-27 20:41:58 +0100

    Better styles

--- public/js/render-slots.js
+++ public/js/render-slots.js
@@ -1,16 +1,27 @@
-var gridWidth = 620;
+var eventPadding = 5;
+var gridAvailableWidth = 615;
+var eventBorder = 3;
 
 function renderSlots(slots) {
   slots.forEach(function (slot) {
-    var eventWidth = (gridWidth / (slot.columns));
+    var eventWidth = (gridAvailableWidth / (slot.columns));
 
     slot.columnatedEvents.forEach(function (ev) {
-      var $ev = $('<div class="event">');
+      var $ev = sampleEvent();
 
-      $ev.css({top: ev.start, height: ev.end - ev.start, width: eventWidth, left: ((ev.column - 1) * eventWidth)});
+      $ev.css({
+        top: ev.start,
+        height: ev.end - ev.start,
+        width: eventWidth - (eventPadding + eventBorder),
+        left: (((ev.column - 1) * eventWidth) + eventPadding)
+      });
 
       $('.grid-container').append($ev);
     });
   });
+
+  function sampleEvent() {
+    return $('<div class="event"><div class="event-desc"><div class="event-title">Sample item</div><div class="event-location">Sample location</div></div></div>');
+  }
 }
 
